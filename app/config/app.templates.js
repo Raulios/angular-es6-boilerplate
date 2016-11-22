98bec57561aae91a8419bdd30505a4db8a4f62e9
Raúl Molina <raulmolina88@gmail.com>
2016-11-22 12:46:14 +0100

    back to previous gitignore config and deleted generated app.templates.js file

--- app/config/app.templates.js
+++
@@ -1,4 +0,0 @@
-angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("home/home.html","<div class=\"test-div\">\nYO\n</div>");
-$templateCache.put("layout/app-view.html","<app-header></app-header>\n\n<div ui-view></div>\n\n<app-footer></app-footer>");
-$templateCache.put("layout/footer.html","");
-$templateCache.put("layout/header.html","");}]);
